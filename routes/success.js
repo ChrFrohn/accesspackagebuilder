fab4e0fda20dffe3126ff27cd6b024aebf1eb94e
nicowyss <wyss.nico@bluewin.ch>
2025-05-28 14:55:20 +0200

    fix success

--- routes/success.js
+++ routes/success.js
@@ -2,37 +2,45 @@ const express = require("express");
 const router = express.Router();
 const { getUserInfo } = require("../auth"); // Import functions from core auth.js
 const insightsClient = require("../appInsightsClient");
-
+const crypto = require("crypto");
 
 // Route to handle the success page after login
-router.get('/success', async (req, res) => {
+router.get("/success", async (req, res) => {
   if (!req.session.token || !req.session.tenantId) {
-    console.error('Access token or tenant ID not found in session');
-    return res.redirect('/'); // Redirect to home if session data is missing
+    console.error("Access token or tenant ID not found in session");
+    return res.redirect("/"); // Redirect to home if session data is missing
   }
 
   const accessToken = req.session.token;
 
   try {
     if (insightsClient) {
+      const hashedId = crypto
+        .createHash("sha256")
+        .update(req.session.tenantId + ":" + req.session.userPrincipalName)
+        .digest("hex");
+
+      insightsClient.setAuthenticatedUserContext(hashedId);
+
       insightsClient.trackEvent({
         name: "SuccessPageVisited",
-        properties: { page: req.originalUrl }
+        properties: { page: req.originalUrl },
       });
-    }  
+    }
+
     // Fetch user list using the access token
     const users = await getUserInfo(accessToken);
     const connectionString = process.env.APPLICATIONINSIGHTS_CONNECTION_STRING;
 
-    res.render('success', {
+    res.render("success", {
       tenantId: req.session.tenantId,
       displayName: req.session.displayName,
       userPrincipalName: req.session.userPrincipalName,
-      connectionString: connectionString // Pass the connectionString to the EJS view
+      connectionString: connectionString, // Pass the connectionString to the EJS view
     });
   } catch (error) {
-    console.error('Error fetching users list:', error); // Debugging statement
-    res.status(500).send('Error fetching users list');
+    console.error("Error fetching users list:", error); // Debugging statement
+    res.status(500).send("Error fetching users list");
   }
 });
 
